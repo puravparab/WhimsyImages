3b899159f2343e90c2fb5b983923807624207073
Purav <puravparabdev@gmail.com>
2023-11-09 00:51:24 -0800

    updates to homepage

--- client/app/page.js
+++ client/app/page.js
@@ -1,70 +1,107 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+
 import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+	const [tswift, setTswift] = useState(0)
+	const [tswiftStyle, setTswiftStyle] = useState("Studio Ghibli")
+	const [mj, setMj] = useState(0)
+	const [mjStyle, setMjStyle] = useState("8-Bit")
+
+	useEffect(() => {
+		// Change tswift image every 5 seconds
+		const tswiftInterval = setInterval(() => {
+			handleImgChange("tswift", (tswift + 1) % 3);
+		}, 4000);
+		// Clean up intervals when the component is unmounted
+		return () => {
+			clearInterval(tswiftInterval);
+		};
+	}, [tswift]);
+
+	useEffect(() => {
+		// Change mj image every 7 seconds
+		const mjInterval = setInterval(() => {
+			handleImgChange("mj", (mj + 1) % 2);
+		}, 12000);
+
+		// Clean up intervals when the component is unmounted
+		return () => {
+			clearInterval(mjInterval);
+		};
+	}, [mj]);
+	
+	// example: tswift, mj
+	// number: 0, 1, 2, etc
+	const handleImgChange = (example, number) => {
+		console.log(number)
+		if (example == "tswift"){
+			if (number == 0 || number == 2){setTswiftStyle("Studio Ghibli")}
+			else {setTswiftStyle("8-Bit")}
+			setTswift(number)
+		} else if (example == "mj"){
+			if (number == 1){setMjStyle("8-Bit")}
+			else {setMjStyle("Studio Ghibli")}
+			setMj(number)
+		}
+	}
+
+
 	return (
 		<main className={styles.main}>
 			<div className={styles.mainTitle}>
-				<h2>Reimagine your images</h2>
+				<h2>Reimagine your Images</h2>
 				<p>Powered by gpt4 and dalle-3</p>
 			</div>
 
 			<div className={styles.mainExample}>
 				<div className={styles.imageContainer}>
-					<img 
-						src="/images/tswift.jpg" 
-						width={790}
-						height={699}
-					/>
+					<Image src="/images/tswift.jpg" width={790} height={699} priority/>
 				</div>
 				<div className={styles.exampleTransition}>
-					{/* <span>Reimagine</span> */}
 					<Image
 						src="/icons/white_arrow.svg"
 						width={200}
 						height={200}
 					/>
 					<span>In the style of</span>
-					<span>"Studio Ghibli"</span>
+					<span>"{tswiftStyle}"</span>
 				</div>
 
 				<div className={styles.imageContainer}>
-					<img 
-						src="/images/tswift_ghibli2.png" 
-						width={800}
-						height={800}
-					/>
+					{tswift === 0 && <Image src="/images/tswift_ghibli2.png" width={800} height={800} />}
+					{tswift === 1 && <Image src="/images/tswift_8bit.png" width={800} height={800} />}
+					{tswift === 2 && <Image src="/images/tswift_ghibli.png" width={800} height={800} />}
 				</div>
 			</div>
+
+			{/* <button onClick={() => {handleImgChange("tswift", (tswift + 1) % 3)}}>Next</button> */}
 
 			<div className={styles.mainExample}>
 				<div className={styles.imageContainer}>
-					<img 
-						src="/images/mj.jpg" 
-						width={790}
-						height={699}
-					/>
+					<Image src="/images/mj.jpg" width={512} height={376} />
 				</div>
 				<div className={styles.exampleTransition}>
-					{/* <span>Reimagine</span> */}
 					<Image
 						src="/icons/white_arrow.svg"
 						width={200}
 						height={200}
 					/>
 					<span>In the style of</span>
-					<span>"8-bit"</span>
+					<span>"{mjStyle}"</span>
 				</div>
 
 				<div className={styles.imageContainer}>
-					<img 
-						src="/images/mj_8bit.png" 
-						width={800}
-						height={800}
-					/>
+					{mj === 0 && <Image src="/images/mj_8bit.png" width={800} height={800} />}
+					{mj === 1 && <Image src="/images/mj_ghibli.png" width={800} height={800} />}
 				</div>
 			</div>
-			
+
+			{/* <button onClick={() => {handleImgChange("mj", (mj + 1) % 2)}}>next</button> */}
+
 		</main>
 	)
 }
